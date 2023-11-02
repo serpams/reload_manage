8ffe6f53ab3f5ba695e946c8a341c6908dd2ff16
Jean Oliveira <eanomaraes@gmail.com>
2023-11-01 23:02:44 -0300

    reverse

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Counter;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,14 +22,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
    return redirect('/transactions');
 });
-
-Route::redirect('/transactions', '/app')
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-
-// require __DIR__ . '/auth.php';
