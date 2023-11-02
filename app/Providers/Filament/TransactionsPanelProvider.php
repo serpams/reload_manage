d3656dc097924068ecfbe6814fe7d2d669b6d0f7
Jean Oliveira <eanomaraes@gmail.com>
2023-11-02 09:33:53 -0300

    refix lg

--- app/Providers/Filament/TransactionsPanelProvider.php
+++ app/Providers/Filament/TransactionsPanelProvider.php
@@ -25,7 +25,7 @@ class TransactionsPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->brandLogo(asset('images/logo.svg'));
+            ->brandLogo(asset('images/logo.svg'))
             ->id('transactions')
             ->path('transactions')
             ->login()
