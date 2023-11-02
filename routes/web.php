7ba8831a52d07d835924bb27249b48805e9327f5
Jean Oliveira <eanomaraes@gmail.com>
2023-11-01 23:00:07 -0300

    new rote

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', function () {
    return redirect('/transactions');
 });
 
+Route::redirect('/transactions', '/app')
+
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
